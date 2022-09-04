f20390ad2c15a2f8272cb20b18bbe92bb3bb82cf
OzcanGuler <gulerozcan818@gmail.com>
2022-09-04 18:18:19 +0300

    DataValidation

---
+++ Appointment/Appointment/Controllers/AppointmentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appointment.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowAppointmentDetails(Models.AppointmentModel appointment)
+        {
+            if (appointment.doctorLastName == null)
+            {
+                appointment.doctorLastName = "Tarik Onur Tiryaki";
+            }
+            if (ModelState.IsValid)
+            {
+                return View("ShowAppointmentDetails", appointment);
+            }
+            else
+            {
+                return View("Index");
+            }
+           
+        }
+    }
+
+}
---
+++ Appointment/Appointment/Models/AppointmentModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Appointment.Models
+{
+    public class AppointmentModel
+    {
+        public AppointmentModel(string patientName, DateTime appointmentDate, decimal patientNetWorth, string doctorLastName, int currentPainLevel)
+        {
+            this.patientName = patientName;
+            this.appointmentDate = appointmentDate;
+            this.patientNetWorth = patientNetWorth;
+            this.doctorLastName = doctorLastName;
+            this.currentPainLevel = currentPainLevel;
+        }
+        public AppointmentModel()
+        {
+
+        }
+        [Required]
+        [StringLength(20,MinimumLength =2)]
+        [DisplayName("Patient's Full Name")]
+        public string patientName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Choose the desired date for your next visit")]
+        public DateTime appointmentDate { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [DisplayName("How much money do you have?")]
+        public decimal patientNetWorth { get; set; }
+
+        [DisplayName("What is the name of doctor you wish to see?")]
+        public string doctorLastName { get; set; } = "Tarik Onur Tiryaki" ;
+        [Required]
+        [Range(0,10)]
+        [DisplayName("What is your cuurent pain level (0 to 10)")]
+        public int currentPainLevel { get; set; }
+    }
+}
